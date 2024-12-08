f92fd4aa9c518155225449f6c5c549ec42a17572
Sundaram-Katare <sundaramkatare53@gmail.com>
2024-12-08 10:25:23 +0530

    basic structure

---
+++ index.js
@@ -0,0 +1,45 @@
+const buttonEl = document.getElementById("btn");
+buttonEl.addEventListener("click" , function(){
+    if(buttonEl.innerText == "Light Mode")
+        {
+            lightMode();
+        }
+        else{
+            darkMode();
+        }
+});
+
+function lightMode() {
+    console.log("hi");
+    const allElements = document.querySelectorAll("*");
+    
+   
+    allElements.forEach(function(element) {
+        element.classList.add("light");
+    });
+
+    let content = document.querySelectorAll(".dark");
+
+    content.forEach(function(element) {
+        element.classList.add("light");
+        element.classList.remove("dark");  // Optionally remove the "dark" class
+        buttonEl.innerText = "Dark Mode";
+    });
+}
+
+function darkMode(){
+    console.log("hello");
+    const allElements = document.querySelectorAll("*");
+
+    allElements.forEach(function(element){
+        element.classList.add("dark");
+    });
+
+    let content = document.querySelectorAll(".dark");
+
+    content.forEach(function(element){
+        element.classList.add("dark");
+        const buttonEl = document.getElementById("btn");
+        buttonEl.innerText = "Light Mode";
+    })
+}
