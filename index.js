4c9d03b0cd7004fd8102925981700fc9ba0a7665
Sundaram-Katare <sundaramkatare53@gmail.com>
2025-01-02 01:41:50 +0530

    Project section added and skill section completed

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+
+var i = 0;
+var txt = "Sundaram Katare";
+var speed = 75;
+
+
+function typeWriter(){
+    if(i < txt.length)
+    {
+        document.getElementById("name").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter,speed);
+    }
+}
+
+typeWriter();
 const buttonEl = document.getElementById("btn");
 buttonEl.addEventListener("click" , function(){
     if(buttonEl.innerText == "Light Mode")
@@ -11,6 +27,10 @@ buttonEl.addEventListener("click" , function(){
 
 function lightMode() {
     console.log("hi");
+    updateCount();
+    i = 0;
+    document.getElementById("name").innerHTML = "";
+    typeWriter();
     const allElements = document.querySelectorAll("*");
     
    
@@ -31,10 +51,16 @@ function lightMode() {
 skillEl.forEach(function(element){
     element.classList.add("skillLight");
 })
+
 }
 
 function darkMode(){
     console.log("hello");
+    updateCount();
+    i = 0;
+    document.getElementById("name").innerHTML = "";
+    typeWriter();
+
     const allElements = document.querySelectorAll("*");
 
     allElements.forEach(function(element){
@@ -51,8 +77,26 @@ function darkMode(){
 
     const skillEl = document.querySelectorAll(".skill");
 
-skillEl.forEach(function(element){
-    element.classList.add("skill");
+    skillEl.forEach(function(element){
+    element.classList.remove("skillLight");
 })
 }
 
+function updateCount() {
+const counter = document.getElementById("counter");
+const speed2 = 350;
+
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+
+    const inc = target / speed2;
+
+    if (count < target) {
+        counter.innerText = Math.round(count + inc);
+        setTimeout(updateCount, 1);
+    } else {
+        counter.innerText = target;
+    }
+};
+
+updateCount();
